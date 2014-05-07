d342508012f7c24ae76f62073e47e61fa61a8f61
Claire <admin@Claires-MacBook-Pro.local>
2014-05-07 16:38:02 -0700

    open dev directly instead of doing copyto

--- plot2.R
+++ plot2.R
@@ -1,4 +1,5 @@
 par("mar" = c(5.1, 5.1, 4.1, 2.1))
+par("mfrow" = c(1,1))
 data <- read.csv("data.txt", header=TRUE, sep=";")
 data$DateTime <- strptime(paste(data$Date, data$Time, sep  = " "), "%d/%m/%Y %H:%M:%S")
 plot(data$DateTime, data$Global_active_power, type="l", xlab = "", ylab="Global Active Power (kilowatts)")
